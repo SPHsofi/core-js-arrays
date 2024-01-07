68fa9e5fb85fbd8e8aa60c22ba5f0e333e05088e
SPHsofi <Sofia_pash@mail.ru>
2024-01-07 11:23:33 +0300

    refactor: solved task (26. getHexRGBValues)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -448,8 +448,11 @@ function getIndicesOfOddNumbers(numbers) {
  *    getHexRGBValues([ 0, 255, 16777215]) => [ '#000000', '#0000FF', '#FFFFFF' ]
  *    getHexRGBValues([]) => []
  */
-function getHexRGBValues(/* arr */) {
-  throw new Error('Not implemented');
+function getHexRGBValues(arr) {
+  return arr.map((num) => {
+    const hex = num.toString(16).toUpperCase();
+    return `#${'0'.repeat(6 - hex.length)}${hex}`;
+  });
 }
 
 /**
