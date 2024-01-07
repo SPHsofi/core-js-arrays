1efb8d515ddabc29fca27fc04be269604f4e4dd3
SPHsofi <Sofia_pash@mail.ru>
2024-01-07 11:15:12 +0300

    refactor: solved task (25. getIndicesOfOddNumbers)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -431,8 +431,11 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers
+    .map((value, index) => ({ value, index }))
+    .filter((obj) => obj.value % 2 !== 0)
+    .map((obj) => obj.index);
 }
 
 /**
