7d9e40e82757003c97d6027b01885b8f7bf61e86
SPHsofi <Sofia_pash@mail.ru>
2024-01-07 10:50:04 +0300

    refactor: solved task (23. getFalsyValuesCount)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -391,8 +391,9 @@ function getElementByIndices(arr, indices) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  const res = arr.filter((elem) => !elem);
+  return res.length;
 }
 
 /**
